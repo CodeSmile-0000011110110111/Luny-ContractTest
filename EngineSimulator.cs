a0fcc5b8b6d4c652fb90b1b8f61acfeb7b76d685
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2026-01-26 14:59:45 +0100

    add engine 'find object' with tests

--- EngineSimulator.cs
+++ EngineSimulator.cs
@@ -47,10 +47,10 @@ namespace Luny.ContractTest
 
         public static void GodotTick(double deltaTime = 1.0 / 60.0)
         {
-            Godot.Time.SimulatedFrameCount++;
-            Godot.Time.SimulatedTimeMsec += (ulong)(deltaTime * 1000.0);
+            global::Godot.Time.SimulatedFrameCount++;
+            global::Godot.Time.SimulatedTimeMsec += (ulong)(deltaTime * 1000.0);
 
-            var nodes = Godot.GodotObject._allObjects.OfType<Godot.Node>().ToList();
+            var nodes = global::Godot.GodotObject._allObjects.OfType<global::Godot.Node>().ToList();
             foreach (var node in nodes)
             {
                 if (node.IsInsideTree() && node.CanProcess())
@@ -62,19 +62,23 @@ namespace Luny.ContractTest
 
         public static void Reset()
         {
-            UnityEngine.Object._allObjects.Clear();
-            Godot.GodotObject._allObjects.Clear();
+            LunyEngine.ForceReset_UnityEditorAndUnitTestsOnly();
+            Luny.Unity.Engine.LunyEngineUnityAdapter.ForceReset_UnitTestsOnly();
+            Luny.Godot.Engine.LunyEngineGodotAdapter.ForceReset_UnitTestsOnly();
+
+            UnityEngine.Object.Reset_UnitTestsOnly();
+            global::Godot.GodotObject.Reset_UnitTestsOnly();
 
             // Re-initialize Godot Root
-            var sceneTree = Godot.SceneTree.Instance;
-            Godot.GodotObject._allObjects.Add(sceneTree.Root);
+            var sceneTree = global::Godot.SceneTree.Instance;
+            global::Godot.GodotObject._allObjects.Add(sceneTree.Root);
 
             UnityEngine.Time.time = 0;
             UnityEngine.Time.frameCount = 0;
             UnityEngine.Time.deltaTime = 1f / 60f;
             
-            Godot.Time.SimulatedFrameCount = 0;
-            Godot.Time.SimulatedTimeMsec = 0;
+            global::Godot.Time.SimulatedFrameCount = 0;
+            global::Godot.Time.SimulatedTimeMsec = 0;
         }
     }
 }
---
+++ UnityBridgeTests.cs
@@ -0,0 +1,59 @@
+using NUnit.Framework;
+using UnityEngine;
+using Luny.Engine;
+using Luny.Unity.Engine;
+using System.Linq;
+
+namespace Luny.ContractTest
+{
+    [TestFixture]
+    public class UnityBridgeTests
+    {
+        [SetUp]
+        public void Setup() => EngineSimulator.Reset();
+
+        [Test]
+        public void UnityObjectService_CreateEmpty_RegistersInLuny()
+        {
+            // For this test, use the official Initialize method
+            LunyEngineUnityAdapter.Initialize();
+            
+            // Advance simulation to trigger Awake/Start
+            EngineSimulator.UnityTick(); 
+            
+            var engine = LunyEngine.Instance;
+            Assert.That(engine, Is.Not.Null, "LunyEngine.Instance should not be null after initialization");
+            
+            var obj = engine.Object.CreateEmpty("TestObject");
+            Assert.That(obj, Is.Not.Null, "engine.Object.CreateEmpty should not return null");
+            Assert.That(obj.Name, Is.EqualTo("TestObject"), "Object name should match");
+            
+            // Verify it exists in Luny registry
+            Assert.That(engine.Objects.AllObjects.Contains(obj), Is.True, "Object should be in registry");
+            
+            // Verify it exists in Unity native world
+            var nativeGo = UnityEngine.Object._allObjects.OfType<GameObject>().FirstOrDefault(g => g.name == "TestObject");
+            Assert.That(nativeGo, Is.Not.Null, "Native GameObject should exist");
+        }
+
+        [Test]
+        public void UnityObjectRegistry_GetSingleObject_FindsExistingNativeObject()
+        {
+            LunyEngineUnityAdapter.Initialize();
+            EngineSimulator.UnityTick();
+
+            // Create a native object NOT through Luny
+            var nativeGo = new GameObject("NativeOnly");
+            
+            var engine = LunyEngine.Instance;
+            Assert.That(engine, Is.Not.Null, "LunyEngine.Instance should not be null");
+            
+            // GetSingleObject should find it and register it
+            var lunyObj = engine.Objects.FindByName("NativeOnly");
+            
+            Assert.That(lunyObj, Is.Not.Null, "GetSingleObject should find the native object");
+            Assert.That(lunyObj.Name, Is.EqualTo("NativeOnly"), "Object name should match native name");
+            Assert.That(engine.Objects.AllObjects.Contains(lunyObj), Is.True, "Found object should be registered");
+        }
+    }
+}
