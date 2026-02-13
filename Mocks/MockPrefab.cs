a81dbee815bbfa3d3d5298ce7499dea6c69c7143
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2026-02-13 14:05:08 +0100

    MockPrefab: fix warnings

--- Mocks/MockPrefab.cs
+++ Mocks/MockPrefab.cs
@@ -1,22 +1,14 @@
 using Luny.Engine.Bridge;
-using Luny.Engine.Bridge.Identity;
 using System;
 
 namespace Luny.ContractTest.Mocks
 {
 	public sealed class MockPrefab : LunyPrefab
 	{
-		public LunyAssetID AssetID { get; }
-		public LunyAssetPath AssetPath { get; }
 		public Boolean IsPlaceholder { get; }
 
 		public MockPrefab(Object mockPrefab, LunyAssetPath path, Boolean isPlaceholder = false)
-			: base(mockPrefab, path)
-		{
-			AssetID = mockPrefab.GetHashCode();
-			AssetPath = path;
-			IsPlaceholder = isPlaceholder;
-		}
+			: base(mockPrefab, path) => IsPlaceholder = isPlaceholder;
 
 		public override String ToString() => $"MockPrefab({AssetPath}, ID={AssetID}, Placeholder={IsPlaceholder})";
 		public override T Instantiate<T>() => throw new NotImplementedException();
